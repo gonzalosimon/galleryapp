49944db4d7a36f156f7553441c401419f2d17236
Gonzalo <aguilarsimon01@gmail.com>
2022-01-06 17:29:56 -0300

    Feed page first structure done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import Search from "./Feed/SearchBar.js";
 import "../App.css";
 import Post from "./Feed/Post.js";
-import Masonry from "react-responsive-masonry";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const baseUrl = `https://api.unsplash.com/photos`;
+import { API_KEY } from "./Api";
 
 // var CONFIG = {
 
@@ -20,102 +19,85 @@ const LOAD_STATE = {
   LOADING: "LOADING",
 };
 
-class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      photos: [],
-      totalPhotos: 0,
-      perPage: 18,
-      currentPage: 1,
-      loadState: LOAD_STATE.LOADING,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchPhotos(this.state.currentPage);
-  }
-
-  fetchPhotos(page) {
-    var self = this;
-    const { perPage } = this.state;
-    const options = {
-      params: {
-        client_id: API_KEY,
-        page: page,
-        per_page: perPage,
-      },
-    };
-
-    this.setState({ loadState: LOAD_STATE.LOADING });
-    axios
-      .get(baseUrl, options)
-      .then((response) => {
-        self.setState({
-          photos: response.data,
-          totalPhotos: parseInt(response.headers["x-total"]),
-          currentPage: page,
-          loadState: LOAD_STATE.SUCCESS,
-        });
-      })
-      .catch(() => {
-        this.setState({ loadState: LOAD_STATE.ERROR });
-      });
-  }
-
-  onSearchSubmit = async (term) => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { count: 30, query: term },
-      headers: {
-        Authorization: `Client-ID ${API_KEY}`,
-      },
-    });
-
-    this.setState({ photos: response.data.results });
-  };
-
-  render() {
-    return (
-      <div className="font-Montserrat text-white">
-        <section className="md:p-44 p-16 bg-home-background">
-          <h1 className="text-5xl font-bold">Simon's Gallery</h1>
-          <div className="text-lg	mt-5">
-            <h3>
-              The internet's source of{" "}
-              <a className="underline" href="https://unsplash.com/license">
-                freely-usable images.
-              </a>
-            </h3>
-            <h3> Powered by creators everywhere.</h3>
-          </div>
-          <div className="mt-4">
-            <Search userSubmit={this.onSearchSubmit} />
-            <p className="mt-4">
-              Trending: flower, wallpapers, backgrounds, coffee
-            </p>
-          </div>
-        </section>
-        {this.state.loadState === LOAD_STATE.LOADING ? (
-          <div className="loader"></div>
-        ) : (
-          <List data={this.state.photos} />
-        )}
-      </div>
-    );
-  }
-}
-
-const List = ({ data }) => {
-  const items = data.map((photo) => {
-    return (
-      <div className="p-1 md:py-6 md:px-6 grid-sizer">
-        {" "}
-        <Post key={photo.id} photo={photo} />
-      </div>
-    );
+const Home = () => {
+  const [value, setValue] = useState("green");
+  const [results, setResults] = useState({
+    photos: [],
+    page: 1,
+    perPage: 50,
+    isLoading: false,
   });
 
-  return <Masonry className=" grid grid-cols-3 ">{items}</Masonry>;
+  const fetchPhotos = useCallback((page, perPage) => {
+    API_KEY.photos.list({ page: page, perPage: perPage }).then((data) => {
+      if (data) {
+        let paginatedData = data.response.results;
+        setResults((prev) => ({
+          ...prev,
+          photos:
+            page === 1
+              ? [...paginatedData]
+              : prev.photos.concat([...paginatedData]),
+          isLoading: false,
+        }));
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchPhotos(1, 50);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchPhotos]);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 300 &&
+      !results.isLoading
+    ) {
+      setResults((prev) => ({
+        ...prev,
+        page: prev.page + 1,
+        isLoading: true,
+      }));
+      fetchPhotos(results.page + 1, 50);
+    }
+  };
+  const { photos, isLoading } = results;
+  let loader;
+  if (photos.length < 0 || isLoading) {
+    loader = <h1>Something</h1>;
+  }
+  return (
+    <div className="font-Montserrat text-white">
+      <section className="md:p-44 p-16 bg-home-background">
+        <h1 className="text-5xl font-bold">Simon's Gallery</h1>
+        <div className="text-lg	mt-5">
+          <h3>
+            The internet's source of{" "}
+            <a className="underline" href="https://unsplash.com/license">
+              freely-usable images.
+            </a>
+          </h3>
+          <h3> Powered by creators everywhere.</h3>
+        </div>
+        <div className="mt-4">
+          <Search />
+          <p className="mt-4">
+            Trending: flower, wallpapers, backgrounds, coffee
+          </p>
+        </div>
+      </section>
+      <ResponsiveMasonry className="p-6 md:py-6 md:px-6 grid-sizer" columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry columnsCount={3} gutter="20px">
+          {photos.length &&
+            photos.map((photo, i) => <img key={i} src={photo.urls.small} />)}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
+  );
 };
 
 export default Home;
